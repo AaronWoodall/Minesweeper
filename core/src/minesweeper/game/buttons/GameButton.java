048ef27cd7394c6020b716d66cc49fda2d97883d
Kyle Billiot <kylejbilliot@gmail.com>
2016-11-11 10:17:46 -0700

    Final flag fix?

--- core/src/minesweeper/game/buttons/GameButton.java
+++ core/src/minesweeper/game/buttons/GameButton.java
@@ -18,7 +18,7 @@ import minesweeper.game.screensystem.Screen;
 import minesweeper.models.Board;
 import minesweeper.models.Square;
 
-public class GameButton extends CallbackButton implements InputProcessor {
+public class GameButton extends CallbackButton {
 	
 	private int boardRow, boardCol;
 	private static int NUMBER_OF_REVEALED_BUTTONS = 7;
@@ -67,8 +67,15 @@ public class GameButton extends CallbackButton implements InputProcessor {
 		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
 			thisSquare.setIsRevealed(true);			
 		} else if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT)){
+			boolean originalFlagValue = thisSquare.getIsFlagged();
 			thisSquare.setIsFlagged(!thisSquare.getIsFlagged());
-			board.setFlagCount(board.getFlagCount() - 1);
+			
+			int addition = 1 * (originalFlagValue ? -1 : 1);
+			board.setFlagCount(board.getFlagCount() - addition);	
+			
+			if (originalFlagValue ){
+				thisSquare.setIsRevealed(false);
+			}
 		}
 		correctTexture();
 		GameLogic logic = ((GamePlayScreen)screenReference).getBoardGenerationController()
@@ -79,73 +86,20 @@ public class GameButton extends CallbackButton implements InputProcessor {
 	}
 	
 	public void correctTexture() {
-		if (thisSquare.getIsFlagged()) {
+		if (thisSquare.getIsRevealed()) {
+			if (thisSquare.getHasBomb()) {
+				setTexture(mineTexture);
+			} else if (thisSquare.getSquareValue() == '0') {
+				setTexture(revealedTexture);	
+			} else {
+				setTexture(numberedTextures[thisSquare.getSquareValue() - '1']);
+			}
+		} else if (thisSquare.getIsFlagged()) {
 			setTexture(flagTexture);
-		} else if (thisSquare.getHasBomb()) {
-			setTexture(mineTexture);
-		} else if (thisSquare.getSquareValue() == '0') {
-			setTexture(revealedTexture);	
 		} else {
-			setTexture(numberedTextures[thisSquare.getSquareValue() - '1']);
+			setTexture(hiddenTexture);
 		}
 	}
 
-	@Override
-	public void onRelease() {
-		
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		if(button == Buttons.LEFT){
-            
-        }
-        if(button == Buttons.RIGHT){
-        	
-        }
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 }
